ee7cd1363db558300f15746765593a15473034f7
Kagublanca <reecekagu@gmail.com>
2022-04-27 13:07:47 +0100

    Saving almost complete, figuring out Document.create

--- server.js
+++ server.js
@@ -2,8 +2,40 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+import Document from "./models/Document.js";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://localhost/pastebin", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  const code = `Welcome to the PasteBin clone!
+Use the commands in the top right corner
+to create a new file to share with others!`;
+  //[\n] being a new-line character
+  res.render("code-display", { code });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  try {
+    console.log("Got here" + { ...Document });
+    const document = await Document.create({ value });
+    console.log("Hello this is the id " + document.id);
+  } catch (e) {
+    console.log("Save failed");
+    res.render("new", { value });
+  }
+  console.log(value);
 });
 
 app.listen(3000);
